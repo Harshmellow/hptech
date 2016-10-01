b5b2ad38bc679de84fa66b6414942b503ec9235f
Marcelo Escalante <marceloescalanteherzog@gmail.com>
2016-09-30 23:06:03 -0400

    Update home.js
    Update: home.js (v1.01)
    
    Fixes-
    Added Alert for first time users [Bugged]
    Added Accorion Fx() to projects.html
    Added Draggable fx() to img In projects.html

--- home.js
+++ home.js
@@ -1,21 +1,46 @@
 
 // Under here belongs javascript for Highland Park Technology Club1
-//
+//Displays Alert Message to First time
+/*function getCookie(c_name) {
+    var c_value = document.cookie;
+    var c_start = c_value.indexOf(" " + c_name + "=");
+    if (c_start == -1) {
+        c_start = c_value.indexOf(c_name + "=");
+    }
+    if (c_start == -1) {
+        c_value = null;
+    } else {
+        c_start = c_value.indexOf("=", c_start) + 1;
+        var c_end = c_value.indexOf(";", c_start);
+        if (c_end == -1) {
+            c_end = c_value.length;
+        }
+        c_value = unescape(c_value.substring(c_start, c_end));
+    }
+    return c_value;
+}*/
+//End of Alert Message
 
+ $( function() {
+    $( "#accordion" )
+      .accordion({
+        header: "> div > h3"
+      })
+      .sortable({
+        axis: "y",
+        handle: "h3",
+        stop: function( event, ui ) {
+          // IE doesn't register the blur when sorting
+          // so trigger focusout handlers to remove .ui-state-focus
+          ui.item.children( "h3" ).triggerHandler( "focusout" );
+ 
+          // Refresh accordion to handle new order
+          $( this ).accordion( "refresh" );
+        }
+      });
+  } );
 
-
-$(document).ready(function() {
-   $('.signup-btn').mouseenter(function() {
-       $(this).animate({
-           height: '+=10px'
-       });
-   });
-   $('.signup-btn').mouseleave(function() {
-       $(this).animate({
-           height: '-=10px'
-       }); 
-   });
-   $('.signup-btn').click(function() {
-       $(this).toggle(1000);
-   }); 
-});
+ //Allows drag and drop of HPCODERS logo
+ $(document).ready(function(){
+ 	$(".logoDrag").draggable();
+ });
